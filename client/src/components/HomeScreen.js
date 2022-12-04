4885451c2ef3a5a0ee2c767af621a568b48a06cb
Dr_Leez <Snargo@users.noreply.github.com>
2022-12-04 17:45:06 -0500

    three pages

---
+++ client/src/components/HomeScreen.js
@@ -0,0 +1,49 @@
+import React, { useContext, useEffect } from 'react'
+import { GlobalStoreContext } from '../store'
+import ListCard from './ListCard.js'
+import MUIDeleteModal from './MUIDeleteModal'
+
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography'
+/*
+    This React component lists all the top5 lists in the UI.
+    
+    @author McKilla Gorilla
+*/
+const HomeScreen = () => {
+    const { store } = useContext(GlobalStoreContext);
+
+    useEffect(() => {        
+        store.loadIdNamePairs();
+    }, []);
+
+    let listCard = "";
+    if (store) {
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!RENDERING FOR CURRENTVIEW: " + store.currentView);
+        listCard = 
+            <List sx={{ width: '90%', left: '5%', bgcolor: 'background.paper' }}>
+            {
+                store.idNamePairs.map((pair) => (
+                    <ListCard
+                        key={pair._id}
+                        idNamePair={pair}
+                        selected={false}
+                    />
+                ))
+            }
+            </List>;
+    }
+    return (
+        <div id="playlist-selector">
+            <div id="list-selector-heading">
+            </div>
+            <div id="list-selector-list">
+                {
+                    listCard
+                }
+                <MUIDeleteModal />
+            </div>
+        </div>)
+}
+
+export default HomeScreen;
